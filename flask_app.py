ff168b6f8405eb3ca35fb24d01f305ba771382f4
Colton Booth <colton@boothlinux.ca>
2018-03-24 22:48:22 +0000

    Added database support

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,42 @@
 
-# A very simple Flask Hello World app for you to get started with...
+#Licenced under the GPLv2
 
+
+#IMPORTS
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+
+#APP SETUP
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+
+#DATABASE SETUP
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="maritimerabroad",
+    password="Iamwealthy1",
+    hostname="maritimerabroad.mysql.pythonanywhere-services.com",
+    databasename="maritimerabroad$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+#MODELS
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
 comments = []
 
+
+#REST OF THE APP
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
